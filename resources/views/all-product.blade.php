a4101b3c3edd3b568b896d57b5bda23369724df9
malkhazidartsmelidze <malkhazidartsmelidze@gmail.com>
2020-10-17 14:13:45 +0400

    addeed migrations

---
+++ resources/views/all-product.blade.php
@@ -0,0 +1,1 @@
+THis is all product viw
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/home', '\App\Http\Controllers\PageController@getHomePage');
 Route::get('/contact', '\App\Http\Controllers\PageController@getContactPage');
 Route::get('/about', '\App\Http\Controllers\PageController@getAboutPage');
 // Route::get('/home', [PageController::class, 'getHomePage']);
+
+Route::get('/product/create', '\App\Http\Controllers\ProductController@createProduct');
+Route::get('/product/all', '\App\Http\Controllers\ProductController@viewAllProduct');
